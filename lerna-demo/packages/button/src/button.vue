17a3353879aefe169a53260a8b5caf3a03e323fe
marui01 <marui01@ofo.com>
2018-02-09 16:28:54 +0800

    add dom tree

--- lerna-demo/packages/button/src/button.vue
+++ lerna-demo/packages/button/src/button.vue
@@ -1,3 +1,19 @@
 <template>
-    
+    <div id="button">
+        <div class="buttonWrapper">
+            <div class="button">
+                {{ text }}
+            </div>
+        </div>
+    </div>
 </template>
+
+<script>
+export default {
+    name: 'button'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
